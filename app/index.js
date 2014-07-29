e274462ec901974427404c57538d2b30265daffa
Melanie Fryman <melanie@frymanet.com>
2014-07-28 21:02:51 -0500

    basic page setup & package.json

--- app/index.js
+++ app/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var express = require('express);
+var express = require('express');
 var app = express();
 var morgan = require('morgan');
 
-app.set('views', __dirname + '/views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
-  res.render('home');
+  res.render('index');
 });
 
 app.get('/about', function(req, res){
